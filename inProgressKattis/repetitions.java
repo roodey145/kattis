8d0b62050ea42864b747865d39eaababd3ada825
roodey145 <64523090+roodey145@users.noreply.github.com>
2024-10-10 14:08:20 +0100

    Adjusted the class to consider reminders

--- inProgressKattis/repetitions.java
+++ inProgressKattis/repetitions.java
@@ -2,8 +2,10 @@ package inProgressKattis;
 
 import java.util.Scanner;
 
-public class repetitions{
+public class repetitions {
     private static Scanner _scanner = new Scanner(System.in);
+    private static int usedNumbers = 0;
+
     public static void main(String[] args) {
 
         // System.out.println(calcUpperLimit(2));
@@ -11,65 +13,62 @@ public class repetitions{
         // System.out.println(calcUpperLimit(4));
         // System.out.println(calcUpperLimit(5));
         // if(true) return;
-        int k = Integer.parseInt( _scanner.nextLine() );
-        int n = Integer.parseInt( _scanner.nextLine() );
+        int n = Integer.parseInt(_scanner.nextLine());
+        int k = Integer.parseInt(_scanner.nextLine());
 
-        
         // Number of characters needed to reach continues repetition
         // int initialValue = k*k + k;
         // int nrToContinuesRe = k * (initialValue + 1); // Upper limit
         int nrToContinuesRe = calcUpperLimit(k);
-        System.out.println(nrToContinuesRe);
+        System.out.println("Upper Limit: " + nrToContinuesRe);
 
         int result = calcNrOfOnes(k, k, nrToContinuesRe < n ? nrToContinuesRe : n, nrToContinuesRe);
         int extra = result / k;
-        if(extra <= 1) extra = 0;
+        if (extra <= 1)
+            extra = 0;
+        extra += (n - usedNumbers) / (k + 1);
 
         System.out.println("Results Before: " + (result));
         System.out.println("Results After: " + (result + result / k));
+        System.out.println("Used Numbers: " + (usedNumbers));
         result += extra + (n > nrToContinuesRe ? n - nrToContinuesRe : 0);
 
-        System.out.println( result );
+        System.out.println(result);
     }
 
-    public static int calcUpperLimit(int k)
-    {
+    public static int calcUpperLimit(int k) {
         int upperLimit = k * k + k;
-        
-        for(int i = 1; i < k - 1; i++)
-        {
+
+        for (int i = 1; i < k - 1; i++) {
             upperLimit = upperLimit * k + k;
         }
 
         return upperLimit;
     }
 
-
-    public static int calcNrOfOnes(int initialK, int k, int n, int upperLimit)
-    {
+    public static int calcNrOfOnes(int initialK, int k, int n, int upperLimit) {
         int newK = k + 1; // Because we need (k + 1) characters to find one repetition, i.e. one number 1
-        int initialValue = initialK*k + initialK;
+        int initialValue = initialK * k + initialK;
 
-
-        if(n <= initialValue)
-        {
-            return n / newK;
-        } else{
+        if (n <= initialValue) {
+            int result = n / newK;
+            usedNumbers = result * newK;
+            return result;
+        } else {
             return initialK * calcNrOfOnes(initialK, initialValue, n, upperLimit);
         }
 
-
         // if(initialValue > upperLimit)
         // {
-        //     int repetition = upperLimit / newK;
-        //     int extraValueToAdd = 0;
-        //     if(n > upperLimit)
-        //         extraValueToAdd += n - upperLimit;
-        //     return repetition + extraValueToAdd;
+        // int repetition = upperLimit / newK;
+        // int extraValueToAdd = 0;
+        // if(n > upperLimit)
+        // extraValueToAdd += n - upperLimit;
+        // return repetition + extraValueToAdd;
         // }
         // else if(n <= initialValue)
         // {
-        //     return n / newK;
+        // return n / newK;
         // }
 
         // // There is k number of repetitions
