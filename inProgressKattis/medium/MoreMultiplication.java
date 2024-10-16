65ea09a6a1395de02a7c21ef753db757e3cdf1e0
roodey145 <64523090+roodey145@users.noreply.github.com>
2024-10-16 10:28:39 +0100

    Fullfil the available test cases

--- inProgressKattis/medium/MoreMultiplication.java
+++ inProgressKattis/medium/MoreMultiplication.java
@@ -2,8 +2,10 @@ package inProgressKattis.medium;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MoreMultiplication {
+    private static Scanner _scanner;
     private static String cellBorder = "|";
     private static List<String> rowsCells;
     private static List<int[][]> rowsNumbers;
@@ -11,11 +13,37 @@ public class MoreMultiplication {
     private static String number1 = "";
     private static String number2 = "";
 
+    private static List<String> listOfNumber1;
+    private static List<String> listOfNumber2;
+
     public static void main(String[] args) {
+        _scanner = new Scanner(System.in);
 
-        number1 = "345";
-        number2 = "56";
+        listOfNumber1 = new ArrayList<>();
+        listOfNumber2 = new ArrayList<>();
 
+        // number1 = "345";
+        // number2 = "56";
+
+        while (_scanner.hasNext()) {
+            number1 = _scanner.nextInt() + "";
+            number2 = _scanner.nextInt() + "";
+            if (Integer.parseInt(number1) == 0 || Integer.parseInt(number2) == 0)
+                continue;
+            _scanner.nextLine();
+            listOfNumber1.add(number1);
+            listOfNumber2.add(number2);
+        }
+
+        for (int i = 0; i < listOfNumber1.size(); i++) {
+            number1 = listOfNumber1.get(i);
+            number2 = listOfNumber2.get(i);
+            processCreationOfTable();
+        }
+
+    }
+
+    private static void processCreationOfTable() {
         List<String[]> rowsData = new ArrayList<>();
         String calcResult;
         rowsCells = new ArrayList<>();
@@ -26,25 +54,26 @@ public class MoreMultiplication {
             for (int outNr = number1.length() - 1; outNr >= 0; outNr--) {
                 calcResult = String.valueOf(
                         Integer.parseInt(number1.charAt(outNr) + "") * Integer.parseInt(number2.charAt(innerNr) + ""));
-                rowsData.get(innerNr)[outNr] = createCellBody(calcResult.charAt(0), calcResult.charAt(1));
+
+                char char1 = calcResult.charAt(0);
+                char char2 = calcResult.length() == 2 ? calcResult.charAt(1) : '0';
+
+                if (calcResult.length() == 1) {
+                    char1 = '0';
+                    char2 = calcResult.charAt(0);
+                }
+
+                rowsData.get(innerNr)[outNr] = createCellBody(char1,
+                        char2);
 
                 // Add the numbers to the numbers list
-                rowsNumbers.get(innerNr)[0][outNr] = Integer.parseInt(calcResult.charAt(0) + ""); // Tens
-                rowsNumbers.get(innerNr)[1][outNr] = Integer.parseInt(calcResult.charAt(1) + ""); // Ones
+                rowsNumbers.get(innerNr)[0][outNr] = Integer.parseInt(char1 + ""); // Tens
+                rowsNumbers.get(innerNr)[1][outNr] = Integer.parseInt(char2 + ""); // Ones
                 // System.out.println(rowsData.get(innerNr)[outNr]);
             }
             rowsCells.add(
                     placeNumberVertically(number2.charAt(innerNr) + "", joinCells(rowsData.get(innerNr))));
         }
-
-        // for (int[][] rowNumbers : rowsNumbers) {
-        // for (int i = 0; i < rowNumbers.length; i++) {
-        // for (int j = 0; j < rowNumbers[0].length; j++) {
-        // System.out.print(rowNumbers[i][j]);
-        // }
-        // System.out.print("\n");
-        // }
-        // }
 
         // Go through the "ones" on the right side
         for (int row = rowsCells.size() - 1; row >= 0; row--) {
@@ -76,16 +105,45 @@ public class MoreMultiplication {
         rowsCells.add(newLastRow);
 
         // Add side slashes
-        for (int i = 1; i < rowsCells.size(); i++) {
-            String newRow = addSlashToLeftOfFirstRow(rowsCells.remove(i));
-            rowsCells.add(i, newRow);
-        }
-
-        System.out.println(joinRows());
-        // for (String s : rowsCells) {
-        // System.out.println(s);
+        // for (int i = 1; i < rowsCells.size(); i++) {
+        // String newRow = addSlashToLeftOfFirstRow(rowsCells.remove(i));
+        // rowsCells.add(i, newRow);
         // }
 
+        System.out.println(addOuterBorder(joinRows()));
+    }
+
+    private static void updateRowByAddingSideSlash(int rowIndex) {
+        String newRow = addSlashToLeftOfFirstRow(rowsCells.remove(rowIndex));
+        rowsCells.add(rowIndex, newRow);
+    }
+
+    // private static String removeLastSlashForOne(String ){
+
+    // }
+
+    private static String addOuterBorder(String innerTable) {
+        StringBuilder table = new StringBuilder();
+        table.append(createOuterBorderLine(number1.length()) + "\n");
+        String[] tableRows = innerTable.split("\n");
+
+        for (String row : tableRows) {
+            table.append("|" + row + "|\n");
+        }
+
+        table.append(createOuterBorderLine(number1.length()));
+        return table.toString();
+    }
+
+    private static String createOuterBorderLine(int length) {
+        StringBuilder line = new StringBuilder("+-");
+
+        for (int i = 0; i < length; i++) {
+            line.append("----");
+        }
+
+        line.append("--+");
+        return line.toString();
     }
 
     private static String joinRows() {
@@ -93,7 +151,7 @@ public class MoreMultiplication {
         for (int i = 0; i < rowsCells.size(); i++) {
             joinedRows.append(rowsCells.get(i));
             if ((i + 1) < rowsCells.size()) {
-                joinedRows.append(" " + createHorBorder(number1.length()) + " \n");
+                joinedRows.append(createHorBorder(number1.length()) + "\n");
             }
         }
 
@@ -120,7 +178,7 @@ public class MoreMultiplication {
             newLastRow.append(lastRowData[i] + "\n");
             if (i + 2 == lastRowData.length) {
                 // Add the border
-                newLastRow.append(" " + createHorBorder(number1.length()) + " \n");
+                newLastRow.append(createHorBorder(number1.length()) + "\n");
             }
         }
 
@@ -143,7 +201,7 @@ public class MoreMultiplication {
                 if (indexOffset < 0) { // Reached the end
                     if (sum >= 10) {
                         reminder = sum / 10;
-                        sum -= reminder;
+                        sum -= reminder * 10;
                     }
 
                     appendSumToRow(row, sum, -1);
@@ -171,14 +229,27 @@ public class MoreMultiplication {
         String rowData = rowsCells.get(rowIndex);
         StringBuilder updatedRowData = new StringBuilder();
         if (cellIndex < 0) { // Append to the right side of the last row
+
             String[] rowRows = rowData.split("\n");
             rowRows[0] = " " + rowRows[0] + "\n";
             rowRows[1] = " " + rowRows[1] + "\n";
-            rowRows[2] = sum + rowRows[2] + "\n";
+
+            // Make sure to avoid writing zeros for the last number
+            if (sum == 0 && rowIndex == 0) {
+                rowRows[2] = " " + rowRows[2] + "\n";
+            } else {
+                rowRows[2] = sum + rowRows[2] + "\n";
+            }
 
             for (int i = 0; i < rowRows.length; i++) {
                 updatedRowData.append(rowRows[i]);
             }
+
+            // Add slash to the next row if exists
+            if (rowIndex < rowsCells.size() - 1 && sum != 0) {
+                updateRowByAddingSideSlash(rowIndex + 1);
+            }
+
         } else {
             String[] rowRows = rowData.split("\n");
             rowRows[0] += "\n";
@@ -209,7 +280,10 @@ public class MoreMultiplication {
             for (int i = 0; i < 3; i++) {
                 updatedRowData.append(rowRows[i]);
             }
-            updatedRowData.append(lastLine);
+
+            updatedRowData.append(
+                    (number1.length() == 1 && number2.length() == 1) ? lastLine.toString().replaceFirst("/", " ")
+                            : lastLine);
 
         }
 
@@ -259,10 +333,12 @@ public class MoreMultiplication {
         StringBuilder rowWithHeader = new StringBuilder();
 
         // Add the header
-        rowWithHeader.append(" ");
+        rowWithHeader.append("  ");
         for (int i = 0; i < numbers.length(); i++) {
             rowWithHeader.append(" " + numbers.charAt(i) + "  ");
         }
+
+        rowWithHeader.append(" ");
 
         rowWithHeader.append("\n" + createHorBorder(numbers.length()) + "\n");
 
@@ -272,11 +348,12 @@ public class MoreMultiplication {
     }
 
     private static String createHorBorder(int length) {
-        StringBuilder border = new StringBuilder("+");
+        StringBuilder border = new StringBuilder(" +");
         for (int i = 0; i < length; i++) {
             border.append("---+");
         }
 
+        border.append(" ");
         return border.toString();
     }
 
